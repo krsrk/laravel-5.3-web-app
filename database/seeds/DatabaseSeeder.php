2222142622c17e04d3cd8bad01d4e05b2ce6c62f
Chris Lopez <chris@gtr.com.mx>
2016-08-26 12:22:34 -0500

    Database Seeder cleaning code

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	//factory('App\User', 3)->create();
-        Model::unguard();
-
-  	//$this->call(UserTableSeeder::class);
+  	$this->call(UserTableSeeder::class);
   	$this->call(PostTableSeeder::class);
-  		//$this->call(PostCommentTableSeeder::class);
 
   	Model::reguard();
     }
